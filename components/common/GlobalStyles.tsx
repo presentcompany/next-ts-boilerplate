7b0e77be99b88fb704633c57df81d56625771489
heyjul3s <julian.cy.low@gmail.com>
2021-10-14 10:01:36 +1100

    remove custom swiper styles

--- components/common/GlobalStyles.tsx
+++ components/common/GlobalStyles.tsx
@@ -8,10 +8,6 @@ export default function GlobalStyles(): React.ReactElement {
         @import url("https://use.typekit.net/raf2yfl.css");
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
 
-        .swiper-pagination-bullet-active {
-        background-color:#232BBE;
-        }
-
         html {
           scroll-behavior: smooth;
         }
@@ -19,67 +15,6 @@ export default function GlobalStyles(): React.ReactElement {
         body {
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
-        }
-
-        .gem-card .swiper-container {
-          width: auto !important;
-          padding-bottom: 50px !important;
-
-          @media only screen and (min-width: 48em) {
-            width: auto !important;
-          }
-        }
-
-        .gem-card .swiper-pagination {
-          bottom: 20px !important;
-        }
-
-        .swiper-container {
-          width: calc(100vw - 16px) !important;
-
-          @media only screen and (min-width: 48em) {
-            width: calc(100vw - 24px) !important;
-          }
-        }
-
-        .swiper-slide:not(.quote-swiper) {
-          margin-right: 4px !important;
-          width: calc(100%/1.5) !important;
-
-          @media only screen and (min-width: 500px) {
-            width: calc(100%/2.5) !important;
-          }
-
-          @media only screen and (min-width: 650px) {
-            width: calc(25% - 10px) !important;
-          }
-
-          @media only screen and (min-width: 1000px) {
-            width: calc(20% - 9px) !important;
-          }
-
-          @media only screen and (min-width: 1200px) {
-            width: calc(100%/6 - 8px) !important;
-          }
-
-          @media only screen and (min-width: 1900px) {
-            width: calc(100%/7 - 7px) !important;
-          }
-
-          @media only screen and (min-width: 2400px) {
-            width: calc(100%/8 - 7px) !important;
-          }
-
-          @media only screen and (min-width: 2900px) {
-            width: calc(100%/9 - 6px) !important;
-          }
-
-          @media only screen and (min-width: 3500px) {
-            width: calc(100%/10 - 6px) !important;
-          }
-
-        .swiper-pagination-bullets {
-
         }
       `}
     />
