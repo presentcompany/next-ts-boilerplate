43d86dd8825a78ac5550763b2623f4e112477cfe
heyjul3s <julian.cy.low@gmail.com>
2021-10-15 13:35:33 +1100

    fix typings

--- components/pages/Home/styled/Post.tsx
+++ components/pages/Home/styled/Post.tsx
@@ -3,14 +3,14 @@ import { GridItem } from '@chakra-ui/react';
 import type { AppTheme } from '@/theme/index';
 
 type PostProps = {
-  theme: AppTheme;
+  theme?: AppTheme;
 };
 
 const Post = styled(GridItem)(({ theme }: PostProps) => {
   return {
     border: `1px solid ${theme?.colors?.brand['500']}`,
-    padding: theme.space?.['4'],
-    marginBottom: theme.space?.['4']
+    padding: theme?.space?.['4'],
+    marginBottom: theme?.space?.['4']
   };
 });
 
