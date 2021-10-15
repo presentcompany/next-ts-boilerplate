89e293382041e4f68c4adafaa6417abbed25aeab
heyjul3s <julian.cy.low@gmail.com>
2021-10-15 13:25:20 +1100

    basic object type style SC

--- components/pages/Home/styled/Post.tsx
+++ components/pages/Home/styled/Post.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
 import { GridItem } from '@chakra-ui/react';
+import type { AppTheme } from '@/theme/index';
 
 const Post = styled(GridItem)(({ theme }) => {
   return {
-    border: `1px solid ${theme.colors.brand['500']}`,
+    border: `1px solid ${(theme as AppTheme)?.colors?.black}`,
     marginBottom: '1em'
   };
 });
