9a144822f95ff0ebc07ce6b691817d4146dcf1a4
heyjul3s <julian.cy.low@gmail.com>
2022-01-11 09:22:41 +1100

    remove experimental features

--- next.config.js
+++ next.config.js
@@ -3,10 +3,5 @@ module.exports = {
   images: {
     // *NOTE: AVIF processing will prolong build time, remove if deemed unnecessary
     formats: ['image/avif', 'image/webp']
-  },
-  experimental: {
-    urlImports: ['https://cdn.skypack.dev'],
-    concurrentFeatures: true,
-    serverComponents: true
   }
 };
