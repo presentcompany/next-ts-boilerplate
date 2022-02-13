2307fd931b6b4b9ff2ca2132c01694a101ba3f8d
heyjul3s <julian.cy.low@gmail.com>
2022-02-14 09:49:08 +1100

    common components export update

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,4 +1,8 @@
-export { Layout } from './Layout';
-export { TextField } from './Forms';
-export { GlobalStyles } from './GlobalStyles';
 export { AnchorLink } from './AnchorLink';
+export { FormField } from './FormField';
+export { Label } from './Label';
+export { Layout } from './Layout';
+export { GlobalStyles } from './GlobalStyles';
+export { Input } from './Input';
+export { Recaptcha } from './Recaptcha';
+export { TextField } from './TextField';
