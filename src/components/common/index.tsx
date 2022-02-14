1b7834ce6b62b379fbd11c371b027d8a42bfd727
heyjul3s <julian.cy.low@gmail.com>
2022-02-14 12:31:47 +1100

    update common components export with LabeledTextField

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,8 +1,8 @@
 export { AnchorLink } from './AnchorLink';
 export { FormField } from './FormField';
 export { Label } from './Label';
+export { LabeledTextField } from './LabeledTextField';
 export { Layout } from './Layout';
 export { GlobalStyles } from './GlobalStyles';
 export { Input } from './Input';
 export { Recaptcha } from './Recaptcha';
-export { TextField } from './TextField';
