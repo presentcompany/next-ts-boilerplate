c9af65d6fffb6e4bc53054435598322af0ee093f
heyjul3s <julian.cy.low@gmail.com>
2022-02-22 09:49:07 +1100

    update Newsletter to use new Form component

--- src/components/common/Newsletter.tsx
+++ src/components/common/Newsletter.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Flex, Button } from '@chakra-ui/react';
-import { FormProvider, useForm } from 'react-hook-form';
 import Joi from 'joi';
 import { joiResolver } from '@hookform/resolvers/joi';
-
-import MailchimpSubscribe, {
-  DefaultFormFields
-} from 'react-mailchimp-subscribe';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 import { validator } from '@/utils/formValidationSchema';
+import { Form } from './Form';
 import { LabeledTextField } from './LabeledTextField';
+
+import type { DefaultFormFields } from 'react-mailchimp-subscribe';
 
 export function Newsletter() {
   const validationSchema = Joi.object({
     newsletter_email: validator.email()
-  });
-
-  const form = useForm({
-    resolver: joiResolver(validationSchema)
   });
 
   const onSubmit =
@@ -35,32 +30,33 @@ export function Newsletter() {
         const isMailchimpSubmissionError = status === 'error';
 
         return (
-          <FormProvider {...form}>
-            <form noValidate onSubmit={form.handleSubmit(onSubmit(subscribe))}>
-              {isMailchimpSubmissionError && (
-                <div>Failed to submit. Please try again later.</div>
-              )}
+          <Form
+            config={{ resolver: joiResolver(validationSchema) }}
+            onSubmitForm={onSubmit(subscribe)}
+          >
+            {isMailchimpSubmissionError && (
+              <div>Failed to submit. Please try again later.</div>
+            )}
 
-              {hasSignedUp && (
-                <div>You have successfully signed up for our newsletter</div>
-              )}
+            {hasSignedUp && (
+              <div>You have successfully signed up for our newsletter</div>
+            )}
 
-              {!hasSignedUp && (
-                <Flex alignItems="center">
-                  <LabeledTextField
-                    name="newsletter_email"
-                    placeholder="Enter your email address"
-                    type="email"
-                    labelText="Sign up to our newsletter"
-                  />
+            {!hasSignedUp && (
+              <Flex alignItems="center">
+                <LabeledTextField
+                  name="newsletter_email"
+                  placeholder="Enter your email address"
+                  type="email"
+                  labelText="Sign up to our newsletter"
+                />
 
-                  <Button type="submit" disabled={hasSignedUp}>
-                    Submit
-                  </Button>
-                </Flex>
-              )}
-            </form>
-          </FormProvider>
+                <Button type="submit" disabled={hasSignedUp}>
+                  Submit
+                </Button>
+              </Flex>
+            )}
+          </Form>
         );
       }}
     />
