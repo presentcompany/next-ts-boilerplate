e2431075f7f7000726e4d8ca07b01de877556ebf
heyjul3s <julian.cy.low@gmail.com>
2022-02-22 16:02:28 +1100

    move TConnectFormCallback type to ConnectForm

--- src/components/common/ConnectForm.tsx
+++ src/components/common/ConnectForm.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
-import type { UseFormReturn, FieldValues } from 'react-hook-form';
 
-type TConnectFormProps = {
+import type {
+  FieldValues,
+  FormState,
+  UseFormRegister,
+  UseFormReturn
+} from 'react-hook-form';
+
+export type TConnectFormProps = {
   children(props: UseFormReturn<FieldValues, object>): React.ReactElement;
+};
+
+export type TConnectFormCallback = {
+  formState: FormState<FieldValues>;
+  register: UseFormRegister<FieldValues>;
 };
 
 export function ConnectForm({ children }: TConnectFormProps) {
