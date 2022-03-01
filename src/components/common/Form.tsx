98d192ea1442a7ab6c811c4c2f1a1a929fb400db
heyjul3s <julian.cy.low@gmail.com>
2022-03-01 12:44:04 +1100

    update Form component to handle submission errors

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,22 +1,51 @@
-import { FormProvider, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { FormProvider as HookFormProvider, useForm } from 'react-hook-form';
 
 import type { ReactNode } from 'react';
-import type { UseFormProps } from 'react-hook-form';
+import type { FormProviderProps, UseFormProps } from 'react-hook-form';
 
 type TFormProps = {
   children: ReactNode;
   config: UseFormProps;
-  onSubmitForm: any;
+  onSubmitForm: (values: any) => Promise<void | IOnSubmitResult>;
 };
+interface IOnSubmitResult {
+  FORM_ERROR?: string;
+  [prop: string]: any;
+}
+
+type TFormProviderProps = FormProviderProps & {
+  submissionError?: string | null;
+};
+
+export const FORM_ERROR = 'FORM_ERROR';
+
+const FormProvider = ({ children, ...props }: TFormProviderProps) => (
+  <HookFormProvider {...props}>{children}</HookFormProvider>
+);
 
 export function Form({ children, config, onSubmitForm }: TFormProps) {
   const form = useForm({
     ...config
   });
 
+  // * NOTE: this is for form submission errors that are NOT due to field validation errors eg. connectivity issues, 400 errors, etc.
+  const [formSubmissionError, setFormSubmissionError] = useState<string | null>(
+    null
+  );
+
   return (
-    <FormProvider {...form}>
-      <form noValidate onSubmit={form.handleSubmit(onSubmitForm)}>
+    <FormProvider {...form} submissionError={formSubmissionError}>
+      <form
+        noValidate
+        onSubmit={form.handleSubmit(async (values) => {
+          const result = (await onSubmitForm(values)) || {};
+
+          if (!!result.FORM_ERROR) {
+            setFormSubmissionError(result.FORM_ERROR);
+          }
+        })}
+      >
         {children}
       </form>
     </FormProvider>
