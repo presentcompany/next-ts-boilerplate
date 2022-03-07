d19ba8b986aa42703910d520037a1b3faf960e23
heyjul3s <julian.cy.low@gmail.com>
2022-03-08 10:37:33 +1100

    add yarn add/remove bash scripts to package.json

---
+++ scripts/yarn-add.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+NVM_DIR="$HOME/.nvm"
+
+source $HOME/.bashrc
+source $HOME/.nvm/nvm.sh
+
+if [[ -f ".nvmrc" ]]; then
+    nvm use && yarn add $@
+else
+    nvm use default && yarn add $@
+fi
+
+
+
---
+++ scripts/yarn-remove.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+NVM_DIR="$HOME/.nvm"
+
+source $HOME/.bashrc
+source $HOME/.nvm/nvm.sh
+
+if [[ -f ".nvmrc" ]]; then
+    nvm use && yarn remove $@
+else
+    nvm use default && yarn remove $@
+fi
+
+
+
