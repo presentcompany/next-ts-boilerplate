a69cba405e64c6f5123cb7f0fef02414e6e753c9
heyjul3s <julian.cy.low@gmail.com>
2022-03-09 09:26:34 +1100

    add 664 permissions to .env

--- scripts/env-create.sh
+++ scripts/env-create.sh
@@ -1,5 +1,6 @@
 if [ ! -f .env ]; then \
   echo "No '.env' file found. Creating new '.env' with sample variables..."
+  touch .env && chmod 664 .env &&
   cat << EOF > .env
 NEXT_PUBLIC_APP_NAME='My Web App'
 NEXT_PUBLIC_BASE_URL='http://localhost:3000'
@@ -9,5 +10,5 @@ NEXT_PUBLIC_API='https://jsonplaceholder.typicode.com'
 NEXT_PUBLIC_GOOGLE_RECAPTCHA_API=1234
 EOF
 else 
-  echo "An '.env' file is found. Omitting creation of new '.env' file."
+  echo "An '.env' file is found. Aborting creation of new '.env' file."
 fi; \
