281760af8707f0ba6596c0abdb315f47c5e8d3b6
heyjul3s <julian.cy.low@gmail.com>
2022-03-11 09:52:07 +1100

    rename extractYoutubeVideoHashFromUrl to getYoutubeVideoId

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -1,5 +1,5 @@
-export function extractYoutubeVideoHashFromUrl(url: string) {
+export function getYoutubeVideoId(url: string) {
   const videoHashQueryParamKey = 'v';
   const searchParams = new URL(url).search;
-  return new URLSearchParams(searchParams).getAll(videoHashQueryParamKey);
+  return new URLSearchParams(searchParams).getAll(videoHashQueryParamKey)?.[0];
 }
