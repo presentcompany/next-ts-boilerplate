336e9e2e5545c67d62e82bda195528833f731d31
heyjul3s <julian.cy.low@gmail.com>
2022-03-11 09:57:20 +1100

    comment out coverageThreshold config

--- jest.config.js
+++ jest.config.js
@@ -21,6 +21,16 @@ const customJestConfig = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
   collectCoverage: true,
 
+  // * Uncomment if you wish to apply test threshold
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 90,
+  //     functions: 90,
+  //     lines: 90,
+  //     statements: 90
+  //   }
+  // },
+
   // * Add more setup options before each test is run
   // * setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // * if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
