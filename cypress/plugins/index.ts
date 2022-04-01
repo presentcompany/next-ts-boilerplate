9369dd00ccab14cf5c4a5c3ef8ef52de8dc59281
heyjul3s <julian.cy.low@gmail.com>
2022-04-01 12:45:51 +1100

    rename plugin index to use TS

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -12,11 +12,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+const pluginConfig: Cypress.PluginConfig = (on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+export default pluginConfig;
